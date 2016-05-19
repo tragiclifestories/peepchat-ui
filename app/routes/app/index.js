8f936b6c8a8ed5c9aac02d393f7ad648f085ff64
James Turley <jamesturley@ip-10-51-18-75.eu-west-1.compute.internal>
2016-05-19 15:20:24 +0100

    rooms

--- app/routes/app/index.js
+++ app/routes/app/index.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
 
+const { RSVP, inject } = Ember;
+
 export default Ember.Route.extend({
+  flashMessages: inject.service(),
+
+  actions: {
+    createRoom() {
+      let data = this.get('currentModel.newRoom');
+      let room = this.store.createRecord('room', {name: data.name});
+
+      this.set('currentModel.newRoom.errors', []);
+    
+      room.save().then(() => {
+        this.get('flashMessages').success(`Room ${data.name} created`);
+
+        this.set('currentModel.newRoom.name', '');
+      })
+        .catch(err => {
+          this.store.unloadRecord(room);
+          this.set('currentModel.newRoom.errors', (err.errors || []).mapBy('detail'));
+          this.get('flashMessages').danger(`Problem creating room: ${data.name}`);
+        });
+    },
+    removeRoom(room) {
+      if (window.confirm('Are you sure?')) {
+        room.destroyRecord().then(() => {
+          this.get('flashMessages').success(`Deleted room: ${room.get('name')}`);
+        }).catch(() => {
+          this.get('flashMessages').danger(`Problem deleting room: ${room.get('name')}`);
+        });
+      }
+    }
+  },
+
+  model() {
+    return RSVP.hash({
+      rooms: this.store.findAll('room'),
+      newRoom: {name: '', errors: []}
+    });
+  }
 });
